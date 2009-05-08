b464cba7d59e566642a75b03deca99c31305d86d
yoshinrt <32790691+yoshinrt@users.noreply.github.com>
2009-05-08 07:35:00 +0000

    add shadow param
    git-svn-id: file:///home/yoshi/.svnrepos/vsd/trunk@152 177d47f1-6453-0410-9475-f12f96c2b1a2

---
+++ vsd_filter/def_shadow.h
@@ -0,0 +1,11 @@
+//			enum name
+//				|				初期値
+//				|				|		config 名
+//				|				|		|
+DEF_SHADOW(	SHADOW_METER_CX,	-1,		"meter_cx"	)	// メーター中心座標 X (-1:default)
+DEF_SHADOW(	SHADOW_METER_CY,	-1,		"meter_cy"	)	// メーター中心座標 Y
+DEF_SHADOW(	SHADOW_METER_R,		-1,		"meter_r"	)	// メーター半径
+
+DEF_SHADOW(	SHADOW_G_SCALE,		1200,	"g_scale"	)	// G スケール * 1000
+
+#undef DEF_SHADOW
