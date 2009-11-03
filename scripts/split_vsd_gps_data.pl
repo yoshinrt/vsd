c7b06661766fc47715019caaad24f7119675a088
yoshinrt <32790691+yoshinrt@users.noreply.github.com>
2009-11-03 03:58:34 +0000

    split_vsd_gps_data.pl 追加
    git-svn-id: file:///home/yoshi/.svnrepos/vsd/trunk@229 177d47f1-6453-0410-9475-f12f96c2b1a2

---
+++ scripts/split_vsd_gps_data.pl
@@ -0,0 +1,51 @@
+#!/bin/perl -w
+
+$TSC = -1;
+$GPSFreq = 16030000 / 65536 / 16;
+
+while( <> ){
+	++$TSC;
+	
+	if( s/.*GPS// ){
+		@_ = split;
+		
+		$_ = sprintf(
+			'$GPRMC,%010.3f,A,' .	# time
+			'%f,N,' .	# lat
+			'%f,E,' .	# long
+			'%f,' .		# knot/h
+			'%f' .		# bearing
+			",010109,,,A",
+			
+			# time
+			int( $TSC / $GPSFreq / 3600 ) * 10000 +
+			int( $TSC / $GPSFreq / 60 ) % 60 * 100 +
+			fmod( $TSC / $GPSFreq, 60 ),
+			
+			# lat
+			int( $_[ 0 ] ) * 100 + fmod( $_[ 0 ], 1 ) * 60,
+			
+			# long
+			int( $_[ 1 ] ) * 100 + fmod( $_[ 1 ], 1 ) * 60,
+			
+			# knot/h
+			$_[ 2 ] / 1.85200,
+			
+			# bearing
+			$_[ 3 ]
+		);
+		
+		$Sum = 0;
+		for( $i = 1; $i < length( $_ ); ++$i ){
+			$Sum ^= ord( substr( $_, $i, 1 ));
+		}
+		
+		printf( "$_*%02X\n", $Sum );
+	}
+}
+
+sub fmod {
+	my( $a, $b ) = @_;
+	
+	return $a - int( $a / $b ) * $b;
+}
