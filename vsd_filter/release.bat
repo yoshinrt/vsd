ffdd5746f9f49bb696776e2984f87a718549bccb
yoshinrt <32790691+yoshinrt@users.noreply.github.com>
2009-11-28 08:16:37 +0000

    manual.lnk も登録
    git-svn-id: file:///home/yoshi/.svnrepos/vsd/trunk@253 177d47f1-6453-0410-9475-f12f96c2b1a2

--- vsd_filter/release.bat
+++ vsd_filter/release.bat
@@ -3,13 +3,14 @@
 mkdir zrelease
 mkdir zrelease\avisynth
 
-copy "C:\Program Files\AVIUTL\vsd_filter.auf" zrelease\vsd_filter_gps.auf
-copy "C:\Program Files\AviSynth 2.5\plugins\vsd_filter_avs.dll" zrelease\avisynth\vsd_filter_gps.dll
-
 pushd zrelease
+
+copy "C:\Program Files\AVIUTL\vsd_filter.auf" vsd_filter_gps.auf
+copy "C:\Program Files\AviSynth 2.5\plugins\vsd_filter_avs.dll" avisynth\vsd_filter_gps.dll
+copy ..\manual.url .
 
 lha a -d ..\vsd_filter_gps.lzh *
 
 popd
 
-del /s/y zrelease
+rmdir /s/q zrelease
