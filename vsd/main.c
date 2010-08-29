3aa950263420cdfd2dce5fd2957bec2618f2368c
yoshinrt <32790691+yoshinrt@users.noreply.github.com>
2010-08-29 05:50:15 +0000

    最小 ROM に戻す
    git-svn-id: file:///home/yoshi/.svnrepos/vsd/trunk@308 177d47f1-6453-0410-9475-f12f96c2b1a2

--- vsd/main.c
+++ vsd/main.c
@@ -31,16 +31,6 @@
 /*** extern *****************************************************************/
 /*** gloval vars ************************************************************/
 
-/*** スピード・タコのみ出力 *************************************************/
-
-INLINE void OutputSerialS( void ){
-	UCHAR c = 0xFF;
-	
-	SerialPack( g_Tacho.uVal );
-	SerialPack( g_Speed.uVal );
-	sci_write( &c, 1 );
-}
-
 /*** main *******************************************************************/
 
 #ifdef MONITOR_ROM
@@ -84,9 +74,6 @@ int main( void ){
 		// SIO, sw 等の UserIO 処理
 		if( bProcessUIOFlag = ~bProcessUIOFlag ){
 			ProcessUIO();
-		//}else{
-		//	// スピード・タコ only 出力
-		//	if( g_Flags.bOutputSerial )	OutputSerialS();
 		}
 	}
 }
