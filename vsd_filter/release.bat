1c0cafa9276ddbeb107069f9ff95c6d7c67fa9ba
yoshinrt <32790691+yoshinrt@users.noreply.github.com>
2012-07-06 15:42:27 +0000

    プロジェクト更新
    git-svn-id: file:///home/yoshi/.svnrepos/vsd/trunk@372 177d47f1-6453-0410-9475-f12f96c2b1a2

--- vsd_filter/release.bat
+++ vsd_filter/release.bat
@@ -9,7 +9,7 @@ copy "D:\Program Files\AVIUTL\vsd_filter.auf" vsd_filter_gps.auf
 copy "D:\Program Files\AviSynth 2.5\plugins\vsd_filter_avs.dll" avisynth\vsd_filter_gps.dll
 copy ..\manual.url .
 
-c:\dds\bin\lha a -d ..\vsd_filter_gps.lzh *
+d:\dds\bin\lha a -d ..\vsd_filter_gps.lzh *
 
 popd
 
