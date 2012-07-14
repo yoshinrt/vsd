6238bad9b84f1af3be7743aa3eaaf3496abf60d4
yoshinrt <32790691+yoshinrt@users.noreply.github.com>
2012-07-14 10:56:53 +0000

    split_nmea.hta 追加
    git-svn-id: file:///home/yoshi/.svnrepos/vsd/trunk@375 177d47f1-6453-0410-9475-f12f96c2b1a2

--- vsd_filter/release.bat
+++ vsd_filter/release.bat
@@ -8,6 +8,7 @@ pushd zrelease
 copy "D:\Program Files\AVIUTL\vsd_filter.auf" vsd_filter_gps.auf
 copy "D:\Program Files\AviSynth 2.5\plugins\vsd_filter_avs.dll" avisynth\vsd_filter_gps.dll
 copy ..\manual.url .
+copy ..\split_nmea.hta .
 
 d:\dds\bin\lha a -d ..\vsd_filter_gps.lzh *
 
