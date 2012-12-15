b71b0110fc35fba9d770cee3358ee6137ac6795c
yoshinrt <32790691+yoshinrt@users.noreply.github.com>
2012-12-15 07:39:17 +0000

    スキン修正 あと，前のコミットは，DrawwLine の width バグも修正してた
    git-svn-id: file:///home/yoshi/.svnrepos/vsd/trunk@531 177d47f1-6453-0410-9475-f12f96c2b1a2

--- vsd_filter/vsd_plugins/_standard.js
+++ vsd_filter/vsd_plugins/_standard.js
@@ -53,9 +53,9 @@ function Initialize(){
 	GraphParam = [
 		"Speed",	"%.0f km/h",	0x00FFFF,
 		"Tacho",	"%.0f rpm",		0xFF4000,
-		"Accel",	"%.0f %%",		0x00FF00,
-	//	"Gx",		"%.2f G[lon]",	0x00FF00,
-	//	"Gy",		"%.2f G[lat]",	0xFF00FF,
+	//	"Accel",	"%.0f %%",		0x00FF00,
+		"Gx",		"%.2f G[lon]",	0x00FF00,
+		"Gy",		"%.2f G[lat]",	0xFF00FF,
 	];
 	Vsd.MakeGraphParam( GraphParam );
 }
