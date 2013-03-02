5055308875f99a37ce3d0177ea334c5acbd39bb2
yoshinrt <32790691+yoshinrt@users.noreply.github.com>
2013-03-02 11:47:29 +0000

    TurnR を殺す
    git-svn-id: file:///home/yoshi/.svnrepos/vsd/trunk@565 177d47f1-6453-0410-9475-f12f96c2b1a2

--- vsd_filter/StdAfx.h
+++ vsd_filter/StdAfx.h
@@ -50,9 +50,9 @@
 #define ToRAD		( M_PI / 180 )
 #define SLIDER_TIME	LOG_FREQ
 #define NaN			std::numeric_limits<double>::quiet_NaN()
-//#ifdef GPS_ONLY
+#ifdef GPS_ONLY
 	#define USE_TURN_R
-//#endif
+#endif
 
 static char *StringNew( char *&szDst, const char *szSrc ){
 	if( szDst == szSrc ) return( szDst );
