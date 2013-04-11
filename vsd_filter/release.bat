c6502686e840ebced5072dd81e3ecce530365357
yoshinrt <32790691+yoshinrt@users.noreply.github.com>
2013-04-11 14:15:15 +0000

    AviSynth って GNU ライセンスじゃん（；´д⊂） というわけでリリースパッケージから削除
    git-svn-id: file:///home/yoshi/.svnrepos/vsd/trunk@596 177d47f1-6453-0410-9475-f12f96c2b1a2

--- vsd_filter/release.bat
+++ vsd_filter/release.bat
@@ -3,12 +3,9 @@
 set path=%path%;c:\cygwin\bin
 
 mkdir zrelease
-mkdir zrelease\avisynth
-
 pushd zrelease
 
 copy "D:\Program Files\AVIUTL\vsd_filter.auf" vsd_filter_gps.auf
-copy "D:\Program Files\AviSynth 2.5\plugins\vsd_filter_avs.dll" avisynth\vsd_filter_gps.dll
 copy ..\manual.url .
 copy ..\split_nmea.hta .
 
