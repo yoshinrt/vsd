ba369204a88b5900cc43d4eacc4a9b75b4057819
yoshinrt <32790691+yoshinrt@users.noreply.github.com>
2013-05-11 07:21:23 +0000

    dds_lib を svn:externals に設定
    git-svn-id: file:///home/yoshi/.svnrepos/vsd/trunk@623 177d47f1-6453-0410-9475-f12f96c2b1a2

--- vsd_filter/vsd_plugins/_log_reader/csv_reader.js
+++ vsd_filter/vsd_plugins/_log_reader/csv_reader.js
@@ -16,8 +16,17 @@ LogReaderInfo.push({
 
 function ReadTorqueLiteLog( Files ){
 	
+	// 汎用 CSV リーダ用パラメータ指定例:
+	// ・CSV ファイルの 1行目は項目名が入っていると仮定．
+	// ・「CSV 項目名」に一致する項目があれば，その値を「VSD プロパティ名」
+	//   のプロパティに代入する．
+	// ・「function or 乗数」に数値が指定されている場合は，その数値を乗じてから
+	//   VSD プロパティに代入する．
+	//   function 名が指定されている場合は，CSV から得た文字列を引数として function
+	//   を呼び，function から返された数値を VSD プロパティに代入する．
+	
 	var ParamDef = {
-		// csv 項目名						Vsd プロパティ名	function or 乗数
+		// CSV 項目名						VSD プロパティ名	function or 乗数
 		"Device Time"						: [ "Time",			StrToUTC ],
 		"Speed (OBD)(km/h)"					: [ "Speed",		1 ],
 		"Engine RPM(rpm)"					: [ "Tacho",		1 ],
