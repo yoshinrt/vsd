8cf5da01eb890bf587e9d4ad2996a5f83f6c9d39
yoshinrt <32790691+yoshinrt@users.noreply.github.com>
2013-07-15 00:02:23 +0000

    もとに戻す
    git-svn-id: file:///home/yoshi/.svnrepos/vsd/trunk@657 177d47f1-6453-0410-9475-f12f96c2b1a2

--- vsd_filter/vsd_plugins/gallery/google_maps2.js
+++ vsd_filter/vsd_plugins/gallery/google_maps2.js
@@ -24,7 +24,7 @@ function Initialize(){
 		APIKey: "AIzaSyABCDEFGHIJKLMNOPQRSTUVWXYZabcdefg",
 		
 		// ズームレベルを 0～21 で指定します
-		Zoom: 19,
+		Zoom: 17,
 		
 		// 地図タイプ
 		// roadmap:地図  satellite:航空写真  terrain:地形図  hybrid:地図+航空写真
