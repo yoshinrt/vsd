757a9041f49467701405cd8210f2d089a20feb8b
yoshinrt <32790691+yoshinrt@users.noreply.github.com>
2015-04-25 09:40:35 +0000

    csemaphore 追加
    git-svn-id: file:///home/yoshi/.svnrepos/vsd/trunk@788 177d47f1-6453-0410-9475-f12f96c2b1a2

---
+++ vsd_filter/CSemaphore.h
@@ -0,0 +1,52 @@
+/*****************************************************************************
+	
+	VSD -- vehicle data logger system  Copyright(C) by DDS
+	
+	CSemaphore.h - semaphore
+	
+*****************************************************************************/
+
+#pragma once 
+
+/*** セマフォ操作クラス *****************************************************/
+
+class CSemaphore {
+	
+  public:
+	CSemaphore(){
+		m_hSemaphore = CreateSemaphore( NULL, 1, 1, NULL );
+	}
+	
+	~CSemaphore(){
+		CloseHandle( m_hSemaphore );
+	}
+	
+	void Lock( void ){
+		DebugMsgD( "Semaphore%08X: waiting\n", this );
+		WaitForSingleObject( m_hSemaphore, INFINITE );
+		DebugMsgD( "Semaphore%08X: entering critical section\n", this );
+	}
+	
+	void Release( void ){
+		DebugMsgD( "Semaphore%08X: exiting critical section\n", this );
+		ReleaseSemaphore( m_hSemaphore, 1, NULL );
+	}
+	
+  private:
+	HANDLE m_hSemaphore;
+};
+
+class CSemaphoreLock {
+	
+  public:
+	CSemaphoreLock( CSemaphore *pSemaphore ) : m_pSemaphore( pSemaphore ){
+		m_pSemaphore->Lock();
+	}
+	
+	~CSemaphoreLock(){
+		m_pSemaphore->Release();
+	}
+	
+  private:
+	CSemaphore *m_pSemaphore;
+};
