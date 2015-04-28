98e87ee2f54cea5ce4588fbe24e40ff99a98f24c
yoshinrt <32790691+yoshinrt@users.noreply.github.com>
2015-04-28 11:58:29 +0000

    スキンギャラリー廃止
    git-svn-id: file:///home/yoshi/.svnrepos/vsd/trunk@794 177d47f1-6453-0410-9475-f12f96c2b1a2

--- vsd_filter/release.bat
+++ vsd_filter/release.bat
@@ -8,32 +8,9 @@ call "D:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
 msbuild /p:Configuration=Release make\make.vcxproj
 msbuild /p:Configuration=ReleaseMT /t:Rebuild vsd_filter\vsd_filter.vcxproj
 
-xcopy /s/i release_files zrelease
-pushd zrelease
+set LANG=ja_JP.UTF-8
+make release
 
-copy "D:\Program Files\AVIUTL\Plugins\vsd_filter.auf" vsd_filter_gps.auf
-
-mkdir vsd_plugins
-xcopy /i/s "D:\Program Files\AVIUTL\Plugins\vsd_plugins" vsd_plugins
-
-rmdir /s/q vsd_plugins\gallery
-del vsd_plugins\_log_reader\vsd_log.js
-
-: API ƒL[íœ
-for %%a in ( vsd_plugins/_user_config.js vsd_plugins/streetview.js ) do (
-	perl -pe 's@.*//#DEL#.*\n@@; s@//#REL#@\t@' ../%%a > %%a
-)
-
-: d:\dds\bin\lha a -d ..\vsd_filter_gps.lzh *
-c:\cygwin\bin\zip -r ../vsd_filter_gps.zip *
-
-popd
-
-rmdir /s/q zrelease
-
-perl -ne 'rename( "vsd_filter_gps.zip", "vsd_filter_gps_r$1.zip" ) if( /#define\s+PROG_REVISION\s+(\d+)/ );' rev_num.h
-
-call "D:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
-msbuild /p:Configuration=Release /t:Rebuild vsd_filter\vsd_filter.vcxproj
+msbuild /p:Configuration=Release vsd_filter\vsd_filter.vcxproj
 
 pause
