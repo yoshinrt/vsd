6486db3d4c9044d17129184539838991907332af
yoshinrt <32790691+yoshinrt@users.noreply.github.com>
2015-06-01 14:54:42 +0000

    なんとなく weak symbol もうごいてそう
    git-svn-id: file:///home/yoshi/.svnrepos/vsd/trunk@845 177d47f1-6453-0410-9475-f12f96c2b1a2

--- vsd2/bin/make_entry2.pl
+++ vsd2/bin/make_entry2.pl
@@ -4,13 +4,14 @@ my( $ExceptList );
 my( %ExceptList );
 
 ## ビルドログから重複するシンボルエラーを pick up
-open( $fp, "../exec_sram/List/vsd2.map" );
-while( <$fp> ){
-	if( /duplicate definitions for "(.*)"/ ){
-		$ExceptList{ $1 } = 1;
-	}
-}
-close( $fp );
+#if( open( $fp, "../exec_sram/List/vsd2.map" )){
+#	while( <$fp> ){
+#		if( /duplicate definitions for "(.*)"/ ){
+#			$ExceptList{ $1 } = 1;
+#		}
+#	}
+#	close( $fp );
+#}
 
 ## 除外する関数名
 $ExceptList = <<'EOF';
@@ -28,61 +29,60 @@ foreach ( split( /\n/, $ExceptList )){
 
 $_ = ();
 
-## 使用する obj 名
+## 除外する obj 名
 
 my( $ObjList, %ObjList );
 $ObjList = <<'EOF';
-cortexm3_macro.o
-#hw_config.o
+#cortexm3_macro.o
+hw_config.o
 #main.o
-stm32f10x_flash.o
-stm32f10x_gpio.o
-stm32f10x_it.o
-stm32f10x_nvic.o
-stm32f10x_rcc.o
-stm32f10x_usart.o
-#stm32f10x_vector.o
-usb_core.o
-usb_desc.o
-usb_endp.o
-usb_init.o
-usb_int.o
-usb_istr.o
-usb_mem.o
-usb_prop.o
-usb_pwr.o
-usb_regs.o
-div.o
-exit.o
-#low_level_init.o
-sprintf.o
-xdnorm.o
-xdscale.o
-xprintffull.o
-xsprout.o
-ABImemcpy.o
-DblAdd.o
-DblCmpGe.o
-DblCmpLe.o
-DblDiv.o
-DblMul.o
-DblSub.o
-DblSubNrml.o
-DblToI32.o
-I32DivMod.o
-I32ToDbl.o
-I64DivMod.o
-I64DivZer.o
-IntDivZer.o
-cexit.o
-#cmain.o
-#copy_init.o
-#data_init.o
-memchr.o
-strchr.o
-strlen.o
-#zero_init.o
-exit.o
+#stm32f10x_flash.o
+#stm32f10x_gpio.o
+#stm32f10x_it.o
+#stm32f10x_nvic.o
+#stm32f10x_rcc.o
+#stm32f10x_usart.o
+stm32f10x_vector.o
+#usb_core.o
+#usb_desc.o
+#usb_endp.o
+#usb_init.o
+#usb_int.o
+#usb_istr.o
+#usb_mem.o
+#usb_prop.o
+#usb_pwr.o
+#usb_regs.o
+#div.o
+#exit.o
+low_level_init.o
+#sprintf.o
+#xdnorm.o
+#xdscale.o
+#xprintffull.o
+#xsprout.o
+#ABImemcpy.o
+#DblAdd.o
+#DblCmpGe.o
+#DblCmpLe.o
+#DblDiv.o
+#DblMul.o
+#DblSub.o
+#DblSubNrml.o
+#DblToI32.o
+#I32DivMod.o
+#I32ToDbl.o
+#I64DivMod.o
+#I64DivZer.o
+#IntDivZer.o
+#cexit.o
+cmain.o
+copy_init.o
+data_init.o
+#memchr.o
+#strchr.o
+#strlen.o
+zero_init.o
 EOF
 
 $ObjList =~ s/^#.*\n//gm;
@@ -104,8 +104,6 @@ $_ = <$fp>; # 読み捨て
 
 open( fpOut, "> ../src/rom_entry.s" );
 
-$IdString = '$I' . 'd$';
-
 print fpOut <<"EOF";
 ;*****************************************************************************
 ;	
@@ -113,11 +111,10 @@ print fpOut <<"EOF";
 ;	Copyright(C) by DDS
 ;	
 ;	rom_entry.s -- rom entry point table
-;	$IdString
 ;	
 ;*****************************************************************************
 
-	RSEG ROM_CODE:CODE(2)
+	RSEG FLASH_CODE:DATA(1)
 EOF
 
 my( $tmp );
@@ -137,24 +134,32 @@ while( <$fp> ){
 	}
 	
 	if(
-		/^([\w_]+)\s+([\w]+)\s+(\S+)/ &&
-		defined( $ObjList{ $3 } ) &&
+		/^([\w_]+)\s+0x([\w]+)\s+(\S+)/ &&
+		!defined( $ObjList{ $3 } ) &&
 		!defined( $ExceptList{ $1 } )
 	){
-		push( @SymbolList, "$3\t$1\t$2" );
+		push( @SymbolList, "$2\t$1\t$3" );
 	}
 }
 
-$PrevObj = '';
+$CurPC = 0x080030EC;
 
 for ( sort @SymbolList ){
-	@_ = split( /\t/, $_ );
+	( $Addr, $Symbol, $Obj ) = split( /\t/, $_ );
 	
-	if( $PrevObj ne $_[ 0 ] ){
-		print fpOut ";*** $_[ 0 ] " . '*' x ( 72 - length( $_[ 0 ] )) . "\n";
-		$PrevObj = $_[ 0 ];
+	if( $CurPC < 0x20000000 && hex( $Addr ) >= 0x20000000 ){
+		print( fpOut "\n\tRSEG FLASH_DATA:DATA(1)\n" );
+		$CurPC = hex( $Addr );
 	}
-	print fpOut "\tEXPORT $_[ 1 ]\n$_[ 1 ] = $_[ 2 ]\n";
+	
+	$Offset = sprintf( "0x%X", hex( $Addr ) - $CurPC );
+	$CurPC	= hex( $Addr );
+	print fpOut << "EOF";
+	DS8	$Offset
+
+	PUBWEAK	$Symbol
+$Symbol:\t// 0x$Addr @ $Obj
+EOF
 }
 
 print fpOut "\tEND\n";
