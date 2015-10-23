a32a1676dae0128f0fbc2189cfa0e148aa8b8ec2
yoshinrt <32790691+yoshinrt@users.noreply.github.com>
2015-10-23 10:53:17 +0000

    インストーラ追加
    git-svn-id: file:///home/yoshi/.svnrepos/vsd/trunk@943 177d47f1-6453-0410-9475-f12f96c2b1a2

--- vsd_filter/release.bat
+++ vsd_filter/release.bat
@@ -4,7 +4,7 @@
 set path=%path%;c:\cygwin\bin
 del "D:\Program Files\AVIUTL\Plugins\vsd_filter.auf"
 
-call "D:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
+call "D:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
 msbuild /p:Configuration=Release make\make.vcxproj
 msbuild /p:Configuration=ReleaseMT /t:Rebuild vsd_filter\vsd_filter.vcxproj
 
