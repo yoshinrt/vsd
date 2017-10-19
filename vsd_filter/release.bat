bbbb5dc58062afdafd2e6bf434e213b7995838e0
Yoshi NRT <32790691+yoshinrt@users.noreply.github.com>
2017-10-19 21:34:19 +0900

    git 公開用に path 調整

--- vsd_filter/release.bat
+++ vsd_filter/release.bat
@@ -2,15 +2,20 @@
 : .tab=4
 
 set path=%path%;c:\cygwin\bin
-del "D:\Program Files\AVIUTL\Plugins\vsd_filter.auf"
 
 call "D:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
 msbuild /p:Configuration=Release make\make.vcxproj
 msbuild /p:Configuration=ReleaseMT /t:Rebuild vsd_filter\vsd_filter.vcxproj
+msbuild /p:Configuration=ReleaseMT /t:Rebuild vsd_filter_avs\vsd_filter_avs.vcxproj
 
 set LANG=ja_JP.UTF-8
 make release
 
-msbuild /p:Configuration=Release vsd_filter\vsd_filter.vcxproj
+rmdir /s/q vsd_filter_avs\Release
+rmdir /s/q vsd_filter_avs\ReleaseMT
+rmdir /s/q vsd_filter_avs\Debug
+rmdir /s/q vsd_filter\Release
+rmdir /s/q vsd_filter\ReleaseMT
+rmdir /s/q vsd_filter\Debug
 
 pause
