5debe696f7ccff265eb692e1c88aadd99b4d1fc0
Yoshi NRT <32790691+yoshinrt@users.noreply.github.com>
2019-10-20 21:10:01 +0900

    release.bat を VS2019 化

--- vsd_filter/release.bat
+++ vsd_filter/release.bat
@@ -1,7 +1,7 @@
 @echo off
 : -*- tab-width: 4 -*-
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
 
 set path=%path%;c:\cygwin\bin
 
@@ -18,15 +18,12 @@ pause
 exit /b 0
 
 :clean
-rmdir /s/q Release
-rmdir /s/q ReleaseMT
-rmdir /s/q Debug
-rmdir /s/q vsd_filter_avs\Release
-rmdir /s/q vsd_filter_avs\ReleaseMT
-rmdir /s/q vsd_filter_avs\Debug
 rmdir /s/q vsd_filter\Release
 rmdir /s/q vsd_filter\ReleaseMT
 rmdir /s/q vsd_filter\Debug
+rmdir /s/q vsd_filter_avs\Release
+rmdir /s/q vsd_filter_avs\ReleaseMT
+rmdir /s/q vsd_filter_avs\Debug
 rmdir /s/q dds_lib\dds_lib\Release
 rmdir /s/q dds_lib\dds_lib\ReleaseMT
 rmdir /s/q dds_lib\dds_lib\Debug
